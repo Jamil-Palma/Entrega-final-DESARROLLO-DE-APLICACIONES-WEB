16d88709f5f87d4a4a65cd8fcaaf5fd13ead12a6
Jamil Brian Palma Salazar <jb.palma.salazar@gmail.com>
2020-08-09 23:09:09 -0400

    entrega final, univ. jamil brian palma salazar

---
+++ Implementacion/vendor/laravel/framework/src/Illuminate/Foundation/Auth/RegistersUsers.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Illuminate\Foundation\Auth;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Auth\Events\Registered;
+
+trait RegistersUsers
+{
+    use RedirectsUsers;
+
+    /**
+     * Show the application registration form.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function showRegistrationForm()
+    {
+        if(Auth::user()->is_admin == 0)
+            abort(403);
+        return view('auth.register');
+    }
+
+    /**
+     * Handle a registration request for the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function register(Request $request)
+    {
+        if(Auth::user()->is_admin == 1){
+            $this->validator($request->all())->validate();
+            $user2 = $this->create($request->all());
+            $user2->save();
+            return redirect()->route('home')->with("estado", "registrado");    
+        }
+        abort(403);  
+    }
+
+    /**
+     * Get the guard to be used during registration.
+     *
+     * @return \Illuminate\Contracts\Auth\StatefulGuard
+     */
+    protected function guard()
+    {
+        return Auth::guard();
+    }
+
+    /**
+     * The user has been registered.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  mixed  $user
+     * @return mixed
+     */
+    protected function registered(Request $request, $user)
+    {
+        //
+    }
+}
